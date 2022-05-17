d17d755539dfa65b0eb9ffa871d14f05a7e8efe7
kuntus1 <vlasenko.max@lll.kpi.ua>
2022-05-17 13:17:35 +0300

    Initial commit

---
+++ src/Lab2_PSA/PZ_5.java
@@ -0,0 +1,19 @@
+package Lab2_PSA;
+
+import MyLibrary.Stack;
+
+public class PZ_5 {
+    public static void main(String[] args) {
+        Stack stack = new Stack(10);
+
+    }
+
+    public static void task1() {
+
+    }
+
+    public static void task2() {
+
+    }
+
+}
