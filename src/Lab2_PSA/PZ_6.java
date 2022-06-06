6b3ff1947268148b29cd47f7dc96d6ba4717f23c
kuntus1 <vlasenko.max@lll.kpi.ua>
2022-06-07 00:12:47 +0300

    Update

---
+++ src/Lab2_PSA/PZ_6.java
@@ -0,0 +1,43 @@
+package Lab2_PSA;
+
+import MyLibrary.QueueList;
+import MyLibrary.QueueArray;
+import java.util.Scanner;
+
+public class PZ_6 {
+    public static void main(String[] args) {
+        QueueList queueList = new QueueList();
+        QueueArray queueArray = new QueueArray(10);
+        Scanner input = new Scanner(System.in);
+        int index = 0;
+
+        System.out.print("Введіть індекс видалення елемента з черги: ");
+        try {
+            index = input.nextInt();
+        }catch (Exception E) {
+            System.err.println("Error: input");
+            System.exit(0);
+        }
+        for (int i = 1; i <= 5; i++)
+            queueList.add(i);
+
+        System.out.println("Queue List:");
+        queueList.printQueue();
+        System.out.println("Після видалення елемента з індексом: " + index);
+        queueList.removeIndexOf(index);
+        queueList.printQueue();
+        System.out.println("Елемент в Queue List з індексом: " + index + " -> " + queueList.get(index));
+
+        for (int i = 1; i <= 5; i++)
+            queueArray.add(i*2);
+
+        System.out.println("\nQueue Array(кільцева):");
+        queueArray.printQueue();
+
+        System.out.println("Після видалення елемента з індексом: " + index);
+        queueArray.removeIndexOf(index);
+        queueArray.printQueue();
+        System.out.println("Елемент в Queue Array з індексом: " + index + " -> " + queueArray.get(index));
+    }
+
+}
