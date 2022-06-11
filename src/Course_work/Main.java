ce82fc3f4bfe3b55da5d470d0531edb324232465
kuntus1 <vlasenko.max@lll.kpi.ua>
2022-06-11 14:49:47 +0300

    add output info with color

--- src/Course_work/Main.java
+++ src/Course_work/Main.java
@@ -1,7 +1,6 @@
 package Course_work;
 
 import MyLibrary.QueueList;
-
 import java.util.Scanner;
 
 public class Main {
@@ -9,9 +8,9 @@ public class Main {
         BinaryTree shopMap = new BinaryTree();                              // Tree: <Shop, ShopAddress>
         BinaryTree clientMap = new BinaryTree();                            // Tree: <Client, ClientAddress>
         QueueList deliveryQueue = new QueueList();                          // Черга клієнтів
-        InputData inputData = new InputData();                              // зчитування даних з файлів
+        InputData inputData = new InputData();                              // Зчитування даних з файлів
 
-        /* create graph after read inputData in Files */
+        /* create Graph after read inputData in Files */
         Graph graph = inputData.readFiles(shopMap, clientMap, deliveryQueue);
 
         /* заповнення списка вершин Graph з Binary Tree (clientMap, shopMap) */
@@ -25,46 +24,46 @@ public class Main {
     public static void start(Graph graph, BinaryTree shopMap, BinaryTree clientMap, QueueList deliveryQueue) {
         Scanner input = new Scanner(System.in);
         while (true) {
-            System.out.print("""   
+            System.out.print(Blue + """   
                              
-                    Обробити доставку наступного клієнта: <1>
-                    Черга замовлень: <2>
-                    Вершини графа: <3>
-                    Таблиця магазинів <4>
-                    Таблиця клієнтів <5>
-                    Матриця суміжності: <6>
-                    Розірвати вершину графа: <7>
-                    Закрити програму: <Another Key>
-                    """);
+                    Обробити доставку наступного клієнта:   <1>
+                    Черга доставки:                         <2>
+                    Вершини графа:                          <3>
+                    Таблиця Shops:                          <4>
+                    Таблиця Clients:                        <5>
+                    Матриця суміжності:                     <6>
+                    Розірвати вершину графа:                <7>
+                    Закрити програму:                    <Another>
+                    """ + Reset);
             String var = input.nextLine();
             switch (var) {
                 case "1" -> {
                     /* After successfully delivery -> remove Client from deliveryQueue */
-                    System.out.println("\nОбробка наступного замовлення:");
+                    System.out.println("\n" + Yellow + "Обробка наступного замовлення: Client -> Shop" + Reset);
                     nextDelivery(graph, shopMap, clientMap, deliveryQueue);
                 }
                 case "2" -> {
-                    System.out.println("\nЧерга клієнтів на доставку:");
+                    System.out.println("\n" + Yellow + "Черга клієнтів на доставку:" + Reset);
                     deliveryQueue.print();
                 }
                 case "3" -> {
-                    System.out.println("\nВершини Graph:");
+                    System.out.println("\n" + Yellow + "Вершини Graph:" + Reset);
                     graph.vertexList.print();
                 }
                 case "4" -> {
-                    System.out.println("\nBinaryTree<Shops> sorted by 'Key'");
+                    System.out.println("\n" + Yellow + "BinaryTree<Shops> sorted by 'Key'" + Reset);
                     shopMap.print();
                 }
                 case "5" -> {
-                    System.out.println("\nBinaryTree<Clients> sorted by 'Key'");
+                    System.out.println("\n" + Yellow + "BinaryTree<Clients> sorted by 'Key'" + Reset);
                     clientMap.print();
                 }
                 case "6" -> {
-                    System.out.println("\nМатриця суміжності Graph:");
+                    System.out.println("\n" + Yellow + "Матриця суміжності Graph:" + Reset);
                     graph.printMatrix();
                 }
                 case "7" -> {
-                    System.out.print("Введіть номер(id) вершини для видалення: ");
+                    System.out.print(Yellow + "Введіть номер(id) вершини для видалення: " + Reset);
                     String delVertex = input.nextLine();
                     graph.removeVertex(Integer.parseInt(delVertex));
                 }
@@ -74,26 +73,24 @@ public class Main {
                 }
             }
         }
-
-
     }
 
     public static void nextDelivery(Graph graph, BinaryTree shopMap, BinaryTree clientMap, QueueList deliveryQueue) {
         if (deliveryQueue.getHead() != null) {                                   // поки є клієнти в черзі замовлень
-            QueueList.Node order = deliveryQueue.remove();                       // дістається клієнт і видаляється
+            QueueList.Node order = deliveryQueue.remove();                       // дістається замовлення і видаляється
             String clientAddress = clientMap.getValue((String) order.key);
             String shopAddress = shopMap.getValue((String) order.value);
 
-            System.out.printf("Client: '%s'\t\tAddress: %s\nShop: '%s'\t\tAddress: %s\n",
+            System.out.printf("Client:\t%5s\t\tAddress: %s\nShop:\t%5s\t\tAddress: %s\n",
                     order.key, clientAddress, order.value, shopAddress);
 
-            System.out.println("\nDFS(пошук у глибину):");
+            System.out.println("\n" + Yellow + "DFS(пошук у глибину): Client -> Shop" + Reset);
             graph.dfs(clientAddress, shopAddress);
 
-            System.out.println("\nBFS(пошук у ширину):");
+            System.out.println("\n" + Yellow + "BFS(пошук у ширину): Client -> Shop" + Reset);
             graph.bfs(clientAddress, shopAddress);
 
-            System.out.println("\nDijkstra:");
+            System.out.println("\n" + Yellow + "Dijkstra:(мінімальна відстань) Shop -> Client" + Reset);
             graph.dijkstra(clientAddress, shopAddress);
 
             /*
@@ -105,7 +102,7 @@ public class Main {
             System.out.println("All orders are delivered");
     }
 
-//    public static BinaryTree getShopMap(){
-//        return shopMap;
-//    }
+    static final String Blue = "\u001B[96m";
+    static final String Yellow = "\u001B[93m";
+    static final String Reset = "\u001B[0m";
 }
