a14b4ce4f523442498d62752b6603349d1c28086
kuntus1 <vlasenko.max@lll.kpi.ua>
2022-06-12 21:46:10 +0300

    update removerVertex, delete comments

--- src/Course_work/Main.java
+++ src/Course_work/Main.java
@@ -23,18 +23,19 @@ public class Main {
 
     public static void start(Graph graph, BinaryTree shopMap, BinaryTree clientMap, QueueList deliveryQueue) {
         Scanner input = new Scanner(System.in);
+
         while (true) {
             System.out.print(Blue + """   
                              
-                    Обробити доставку наступного клієнта:   <1>
-                    Черга доставки:                         <2>
-                    Вершини графа:                          <3>
-                    Таблиця Shops:                          <4>
-                    Таблиця Clients:                        <5>
-                    Матриця суміжності:                     <6>
-                    Розірвати вершину графа:                <7>
-                    Закрити програму:                    <Another>
+                    <1>         Обробити доставку наступного клієнта:
+                    <2>         Черга доставки:
+                    <3>         Вершини графа:
+                    <4>         Таблиця Shops:
+                    <5>         Таблиця Clients:
+                    <6>         Матриця суміжності:
+                    <Another>   Закрити програму:
                     """ + Reset);
+
             String var = input.nextLine();
             switch (var) {
                 case "1" -> {
@@ -48,7 +49,7 @@ public class Main {
                 }
                 case "3" -> {
                     System.out.println("\n" + Yellow + "Вершини Graph:" + Reset);
-                    graph.vertexList.print();
+                    graph.printVertex();
                 }
                 case "4" -> {
                     System.out.println("\n" + Yellow + "BinaryTree<Shops> sorted by 'Key'" + Reset);
@@ -61,11 +62,6 @@ public class Main {
                 case "6" -> {
                     System.out.println("\n" + Yellow + "Матриця суміжності Graph:" + Reset);
                     graph.printMatrix();
-                }
-                case "7" -> {
-                    System.out.print(Yellow + "Введіть номер(id) вершини для видалення: " + Reset);
-                    String delVertex = input.nextLine();
-                    graph.removeVertex(Integer.parseInt(delVertex));
                 }
                 default -> {
                     System.err.println("The program completed successfully");
@@ -80,26 +76,43 @@ public class Main {
             QueueList.Node order = deliveryQueue.remove();                       // дістається замовлення і видаляється
             String clientAddress = clientMap.getValue((String) order.key);
             String shopAddress = shopMap.getValue((String) order.value);
+            Scanner input = new Scanner(System.in);
 
-            System.out.printf("Client:\t%5s\t\tAddress: %s\nShop:\t%5s\t\tAddress: %s\n",
-                    order.key, clientAddress, order.value, shopAddress);
+            if (clientAddress != null && shopAddress != null) {                 // якщо в дереві є Client і Shop
+                System.out.printf("Client:\t%5s\t\tAddress: %s\nShop:\t%5s\t\tAddress: %s\n",
+                        order.key, clientAddress, order.value, shopAddress);
+                algoritmGraph(graph, clientAddress, shopAddress);
 
-            System.out.println("\n" + Yellow + "DFS(пошук у глибину): Client -> Shop" + Reset);
-            graph.dfs(clientAddress, shopAddress);
+                System.out.print(Blue + "Розірвати вершину і перерахувати шлях <1> | <Another>: " + Reset);
+                String var = input.nextLine();
+                if (var.equals("1")) {
+                    System.out.print("\n" + Yellow + "Введіть номер(и) вершини Graph для видалення(id): " + Reset);
+                    String[] delVertex = input.nextLine().split(" ");
+                    for (String vertex : delVertex) {
+                        try {
+                            System.out.printf("%b\t<-\tDeleted vertex: %s\n", graph.removeVertex(Integer.parseInt(vertex)), vertex);
+                        } catch (Exception ignored) {
+                            System.out.printf("false\t<-\tDeleted vertex: %s\n", vertex);
+                        }
+                    }
 
-            System.out.println("\n" + Yellow + "BFS(пошук у ширину): Client -> Shop" + Reset);
-            graph.bfs(clientAddress, shopAddress);
+                    System.out.print(Blue + "\nПерераховані шляхи:" + Reset);
+                    algoritmGraph(graph, clientAddress, shopAddress);
+                }
+            } else System.out.println("Client or Shop not in BinaryTree");
 
-            System.out.println("\n" + Yellow + "Dijkstra:(мінімальна відстань) Shop -> Client" + Reset);
-            graph.dijkstra(clientAddress, shopAddress);
+        } else System.out.println("All orders are delivered");
+    }
 
-            /*
-            Спросить хотите ли разорвать вершину и пересчитать пути?
-            если да -> разорвать и перезапустить.
-             */
+    public static void algoritmGraph(Graph graph, String clientAddress, String shopAddress) {
+        System.out.println("\n" + Yellow + "DFS(пошук у глибину): Client -> Shop" + Reset);
+        graph.dfs(clientAddress, shopAddress);
 
-        } else
-            System.out.println("All orders are delivered");
+        System.out.println("\n" + Yellow + "BFS(пошук у ширину): Client -> Shop" + Reset);
+        graph.bfs(clientAddress, shopAddress);
+
+        System.out.println("\n" + Yellow + "Dijkstra:(мінімальний час) Shop -> Client" + Reset);
+        graph.dijkstra(clientAddress, shopAddress);
     }
 
     static final String Blue = "\u001B[96m";
